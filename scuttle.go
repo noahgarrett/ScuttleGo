72cb9f03d8211320c8822db572a70e89274bec50
Noah Garrett <67662284+noahgarrett@users.noreply.github.com>
2022-07-08 14:00:25 -0400

    possible fix

--- scuttle.go
+++ scuttle.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/noahgarrett/scuttlego/api"
 	"github.com/noahgarrett/scuttlego/riot/lol"
 )
 
@@ -20,13 +22,12 @@ func CreateClient(apiKey string) *Client {
 	return client
 }
 
-//func main() {
-//	//scuttle := CreateClient("RGAPI-918e1b3a-c7f7-4829-9b6e-6731523fc8ac")
-//	//fmt.Println(scuttle.LoL.Summoner.GetByAccountId(api.RegionNorthAmerica, "Hyy3p4ZLAzRI4Nm2etYxRExzrYEM1B-yzs99ciEiZPoz6GfFVUJsSFSm").Id)
-//	//fmt.Println(len(scuttle.LoL.Mastery.GetAllMasteriesBySummoner(api.RegionNorthAmerica, "a5TZtZswIYxOelDN8yRvenmTmeDVNwBRUC01CZnnCP0uE3PrHlbjHbFAkQ")))
-//	//
-//	//fmt.Println(scuttle.LoL.Mastery.GetChampionMasteryBySummoner(api.RegionNorthAmerica, "a5TZtZswIYxOelDN8yRvenmTmeDVNwBRUC01CZnnCP0uE3PrHlbjHbFAkQ", 150).ChampionPoints)
-//	//entries := scuttle.LoL.League.GetEntriesByQueue(api.RegionNorthAmerica, "RANKED_SOLO_5x5", "BRONZE", "I", "1")
-//	//fmt.Println(entries)
-//
-//}
+func main() {
+	scuttle := CreateClient("")
+	fmt.Println(scuttle.LoL.Summoner.GetByAccountId(api.RegionNorthAmerica, "Hyy3p4ZLAzRI4Nm2etYxRExzrYEM1B-yzs99ciEiZPoz6GfFVUJsSFSm").Id)
+	fmt.Println(len(scuttle.LoL.Mastery.GetAllMasteriesBySummoner(api.RegionNorthAmerica, "a5TZtZswIYxOelDN8yRvenmTmeDVNwBRUC01CZnnCP0uE3PrHlbjHbFAkQ")))
+
+	fmt.Println(scuttle.LoL.Mastery.GetChampionMasteryBySummoner(api.RegionNorthAmerica, "a5TZtZswIYxOelDN8yRvenmTmeDVNwBRUC01CZnnCP0uE3PrHlbjHbFAkQ", 150).ChampionPoints)
+	entries := scuttle.LoL.League.GetEntriesByQueue(api.RegionNorthAmerica, "RANKED_SOLO_5x5", "BRONZE", "I", "1")
+	fmt.Println(entries)
+}
